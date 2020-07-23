da6b4892ec69f0beae7d013bd5952d718af0a886
Peter Newton <peter@4sightdesigns.com>
2020-07-23 17:17:54 +0100

    Add black/white lists

--- src/Classes/ScriptState.ts
+++ src/Classes/ScriptState.ts
@@ -1,9 +1,13 @@
-import { StoreState } from "../types";
+import { StoreState, User } from "../types";
 
 export class ScriptState {
   blocking: boolean;
+  blackList: User[];
+  whiteList: User[];
 
   constructor(store: StoreState) {
     this.blocking = store.blocking;
+    this.blackList = [...store.blackList];
+    this.whiteList = [...store.whiteList];
   }
 }
