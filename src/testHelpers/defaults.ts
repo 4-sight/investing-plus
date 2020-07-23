984ce98d416e2fa5ea508d31122b120f3d618f42
Peter Newton <peter@4sightdesigns.com>
2020-07-23 17:15:25 +0100

    Update default store

--- src/testHelpers/defaults.ts
+++ src/testHelpers/defaults.ts
@@ -3,4 +3,32 @@ import { StoreState } from "../types";
 export const store: StoreState = {
   enabled: true,
   blocking: true,
+  blackList: [
+    {
+      name: "test-user-1",
+      id: "1234",
+    },
+    {
+      name: "test-user-2",
+      id: "2345",
+    },
+    {
+      name: "test-user-3",
+      id: "3456",
+    },
+  ],
+  whiteList: [
+    {
+      name: "test-user-4",
+      id: "4567",
+    },
+    {
+      name: "test-user-5",
+      id: "5678",
+    },
+    {
+      name: "test-user-6",
+      id: "6789",
+    },
+  ],
 };
