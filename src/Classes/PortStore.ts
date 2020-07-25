82410f379aaa9ee318037f71d59e90ad0e8684b0
Peter Newton <peter@4sightdesigns.com>
2020-07-25 18:52:10 +0100

    Initial commit

---
+++ src/Classes/PortStore.ts
@@ -0,0 +1,51 @@
+import { Ports, ScriptStateChanges } from "../types";
+import { PortHandler } from "./PortHandler";
+
+export class PortStore {
+  private ports: Ports;
+
+  constructor(initialPortMap?: Ports) {
+    this.ports = initialPortMap || new Map();
+  }
+
+  addPort = (id: number, port: PortHandler) => {
+    this.ports.set(id, port);
+  };
+
+  removePort = (id: number) => (): void => {
+    this.ports.delete(id);
+  };
+
+  enablePorts = () => {
+    this.ports.forEach((port) => {
+      port.enable();
+    });
+  };
+
+  disablePorts = () => {
+    this.ports.forEach((port) => {
+      port.disable();
+    });
+  };
+
+  updatePorts = ([changes, n]: [ScriptStateChanges, number]) => {
+    if (n > 1) {
+      this.ports.forEach((port) => {
+        port.batchUpdate(changes);
+      });
+    } else {
+      this.ports.forEach((port) => {
+        const change = Object.keys(changes)[0];
+
+        switch (change) {
+          case "blocking":
+            if (changes.blocking) {
+              port.blocking.enable();
+            } else {
+              port.blocking.disable();
+            }
+        }
+      });
+    }
+  };
+}
