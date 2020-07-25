cd0e142fee6b0aac3e6166e20f1c3399c648bc8d
Peter Newton <peter@4sightdesigns.com>
2020-07-25 15:36:51 +0100

    Create basic popup

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -4,5 +4,45 @@ import { useStore } from "../hooks";
 export default function Popup() {
   const [store, dispatch] = useStore();
 
-  return null;
+  const toggleBlocking = () => {
+    dispatch({ blocking: !store?.blocking });
+  };
+
+  const toggleEnabled = () => {
+    dispatch({ enabled: !store?.enabled });
+  };
+
+  return (
+    <div className={`popupContainer ${store?.enabled ? "enabled" : "disabled"}`}>
+      Hello, world!
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          toggleEnabled();
+        }}
+        style={{
+          cursor: "pointer",
+          backgroundColor: `${store?.enabled ? "green" : "red"}`,
+          color: `${store?.enabled ? "white" : "black"}`,
+        }}
+      >
+        Toggle Enabled
+      </button>
+      <button
+        className={`blocking-toggle ${store?.blocking ? "enabled" : "disabled"}`}
+        data-testid="blocking-toggle"
+        onClick={(e) => {
+          e.preventDefault();
+          toggleBlocking();
+        }}
+        style={{
+          cursor: "pointer",
+          backgroundColor: `${store?.blocking ? "green" : "red"}`,
+          color: `${store?.blocking ? "white" : "black"}`,
+        }}
+      >
+        Toggle Blocking
+      </button>
+    </div>
+  );
 }
