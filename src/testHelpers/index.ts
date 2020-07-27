1182a737a59bdd01c8e6a97b15405d4d9afb2f31
Peter Newton <peter@4sightdesigns.com>
2020-07-27 17:24:28 +0100

    Add exports

--- src/testHelpers/index.ts
+++ src/testHelpers/index.ts
@@ -1,3 +1,6 @@
 export * from "./mockError";
 export * from "./MockPort";
 export * from "./getProperties";
+export * as defaults from "./defaults";
+export * from "./MockPortHandler";
+export * from "./MockPortStore";
